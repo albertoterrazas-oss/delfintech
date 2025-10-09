01ad4d5fbd9faf2197290da4c74cf7544f06c458
edgarguzmang <20170189@uttcampus.edu.mx>
2025-10-09 14:57:36 -0600

    Proyecto Arriba

---
+++ app/Models/Sistemas/Eventos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Sistemas;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Eventos extends Model
+{
+    use HasFactory;
+}

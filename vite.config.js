da1e79795ff537eb598d69b707a3277b7dbda29e
edgarguzmang <20170189@uttcampus.edu.mx>
2025-11-11 10:09:13 -0600

    ....

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,24 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.jsx',
+//             refresh: true,
+//         }),
+//         react(),
+//     ],
+//     // server: {
+//     //     host: true,       // <--- importante
+//     //     port: 5173,       // <--- puedes cambiarlo si lo necesitas
+//     //     hmr: {
+//     //         host: '192.168.139.46', // <-- tu IP local (no localhost)
+//     //     },
+//     // },
+// });
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
@@ -10,11 +31,9 @@ export default defineConfig({
         }),
         react(),
     ],
-    // server: {
-    //     host: true,       // <--- importante
-    //     port: 5173,       // <--- puedes cambiarlo si lo necesitas
-    //     hmr: {
-    //         host: '192.168.139.46', // <-- tu IP local (no localhost)
-    //     },
-    // },
+    // 👇 Añade esta sección para cambiar el límite de advertencia
+    build: {
+        // Establece el límite de advertencia de tamaño de chunk a 1000 kB (1 MB)
+        chunkSizeWarningLimit: 5000, 
+    },
 });

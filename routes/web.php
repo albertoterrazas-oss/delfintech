883e7a2bed6f9622ca8077d0dd5e0cfb0e4d4a48
edgarguzmang <20170189@uttcampus.edu.mx>
2025-11-14 13:11:15 -0600

    ...

--- routes/web.php
+++ routes/web.php
@@ -17,42 +17,9 @@ use Inertia\Inertia;
 |
 */
 
-// Route::get('/', function () {
-//     return Inertia::render('Home', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register')
-//     ]);
-// });
 
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// Route::get('/', function () {
-//     // Redirigir a dashboard si está autenticado
-//     if (auth()->check()) {
-//         return redirect()->route('dashboard');
-//     }
-//     // Si no está autenticado, mostrar login
-//     return Inertia::render('Auth/Login');
-// })->name('login');
-
-// // Ruta específica de login que también redirige si está autenticado
-// Route::post('/login', function () {
-//     if (auth()->check()) {
-//         return redirect()->route('dashboard');
-//     }
-//     return Inertia::render('Auth/Login');
-// });
-
-  Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login')->middleware('authWeb');;
+Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    ->name('login')->middleware('authWeb');;
 
 Route::post('login', [AuthenticatedSessionController::class, 'store'])
     ->name('login')->middleware('authWeb');
@@ -60,22 +27,6 @@ Route::post('login', [AuthenticatedSessionController::class, 'store'])
 Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
     ->name('logout')->middleware('auth');
 
-// Route::middleware(['auth'])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return Inertia::render('Home', [
-//             'initialPage' => '/dashboard'
-//         ]);
-//     })->name('dashboard');
-
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// Rutas de autenticación y API específicas primero
-// Route::middleware(['auth'])->group(function () {
-//     // Rutas específicas de API o backend...
-// });
 
 // Captura todas las demás rutas y las envía a React Router
 Route::get('{path?}', function () {
